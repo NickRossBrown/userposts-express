006b47dec5516dbeaea7c1592cfcac7a39d3f0e8
Nick Brown <nickrossbrown@gmail.com>
2019-03-21 14:03:36 -0700

    fix cross-origin read blocking (CORB)

--- app.js
+++ app.js
@@ -5,7 +5,16 @@ const feedRoutes = require('./routes/feed');
 const app = express();
 
 app.use(bodyParser.json());
-// application/json
+app.use((req, res, next) => {
+    // set all the url domains that are allowed to access our server
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Let url/domains use specified methods
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    // Allow clients to send specified or auth requests
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    // next => so that the request can now continue and be handled by the routes
+    next();
+})
 app.use('/feed', feedRoutes);
 
 app.listen(8080);
