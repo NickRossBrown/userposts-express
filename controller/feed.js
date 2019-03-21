bf2fc34bb8c133e3e27af4485cb54d4795cf0fff
Nick Brown <nickrossbrown@gmail.com>
2019-03-21 13:47:55 -0700

    add working postman req createPost POST

--- controller/feed.js
+++ controller/feed.js
@@ -3,3 +3,13 @@ exports.getPosts = (req, res, next) => {
         posts: [{title: 'title here', content: 'look at this content, super cool right?'}]
         });
 };
+
+exports.createPost = (req, res, next) => {
+    const title = req.body.title;
+    const content = req.body.content;
+    // create post in db
+    res.status(201).json ({
+        message: 'post was created successfully',
+        post: { id: new Date().toISOString(), title: title, content: content }
+    });
+};
--- routes/feed.js
+++ routes/feed.js
@@ -3,6 +3,9 @@ const feedController =  require('../controller/feed')
 
 const router = express.Router();
 
+// GET /feed/posts
 router.get('/posts', feedController.getPosts);
+// POST /feed/posts
+router.post('/posts', feedController.createPost);
 
 module.exports = router;
