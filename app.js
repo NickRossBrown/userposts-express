23089336bfa3a1315e708e369d9726f4d9640651
Nick Brown <nickrossbrown@gmail.com>
2019-03-23 23:11:26 -0700

    add a custom error handling function

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose  = require('mongoose');
 const feedRoutes = require('./routes/feed');
+const path = require('path');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use('./images', express.static(path.join(__dirname, 'images')));
 app.use((req, res, next) => {
     // set all the url domains that are allowed to access our server
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -17,5 +19,12 @@ app.use((req, res, next) => {
     next();
 })
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({message: message})
+})
 
 mongoose.connect('mongodb+srv://dbUsername:learnAPI@cluster0-ozt5p.mongodb.net/messages?retryWrites=true').then(result => {app.listen(8080);}).catch(err => console.log(err));
--- controller/feed.js
+++ controller/feed.js
@@ -22,9 +22,9 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
-		return res
-			.status(422)
-			.json({ message: 'validation failed', errors: errors.array() });
+		const error = new Error('validation failed');
+		error.statusCode = 422
+		throw error;
 	}
 	const title = req.body.title;
 	const content = req.body.content;
@@ -43,7 +43,10 @@ exports.createPost = (req, res, next) => {
 		});
 	})
 	.catch(err => { 
-		console.log(err)
+		if (!err.statusCode) {
+			err.statusCode = 500;
+		}
+		next(err)
 	})
 	
 };
